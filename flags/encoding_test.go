67a2194387ca2abfde474cf70d23b564d5b3c610
gucchisk <gucchi_sk@yahoo.co.jp>
2021-01-14 23:57:51 +0900

    Fix

---
+++ flags/encoding_test.go
@@ -0,0 +1,49 @@
+package flags
+
+import(
+	"testing"
+	"github.com/gucchisk/bytestring"
+)
+
+func TestNewEncoding(t *testing.T) {
+	type TestData struct {
+		str string
+		expect bytestring.Encoding
+	}
+	tests := []TestData{
+		{
+			str: "ascii",
+			expect: bytestring.Ascii,
+		},
+		{
+			str: "Ascii",
+			expect: bytestring.Ascii,
+		},
+		{
+			str: "hex",
+			expect: bytestring.Hex,
+		},
+		{
+			str: "Hex",
+			expect: bytestring.Hex,
+		},
+		{
+			str: "base64",
+			expect: bytestring.Base64,
+		},
+		{
+			str: "Base64",
+			expect: bytestring.Base64,
+		},
+	}
+
+	for _, data := range tests {
+		enc, err := NewEncoding(data.str)
+		if err != nil {
+			t.Errorf("err: %v", err)
+		}
+		if enc != data.expect {
+			t.Errorf("error")
+		}
+	}
+}
